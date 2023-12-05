33bc78ed1649df67136d0ac5263b4f70e47485ec
Elvan Hidayat <elvanhd@gmail.com>
2023-12-06 06:33:05 +0700

    installing millionjs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import react from '@vitejs/plugin-react';
+import million from 'million/compiler';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [million.vite({ auto: true }), react()],
 });
